fdc001ee7beca086a724f8eabb135ad4505d0ad7
Waqas Ahmed <ahmed.waqas23@gmail.com>
2019-11-23 15:50:26 +0100

    Frontend setup, design base template and setup assets

--- src/ContactBundle/Controller/ContactController.php
+++ src/ContactBundle/Controller/ContactController.php
@@ -5,13 +5,13 @@ namespace ContactBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends Controller
+class ContactController extends Controller
 {
     /**
      * @Route("/")
      */
     public function indexAction()
     {
-        return $this->render('@AddressBookContact/Default/index.html.twig');
+        return $this->render('@AddressBookContact/addContact.html.twig');
     }
 }
